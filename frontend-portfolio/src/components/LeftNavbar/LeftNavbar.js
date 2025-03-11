6b4699cb63a390d97f72c97e4add3346c346168f
Hector Quinones <quinonhector1@gmail.com>
2025-03-11 16:26:37 -0500

    added social and resume links to navabar

--- frontend-portfolio/src/components/LeftNavbar/LeftNavbar.js
+++ frontend-portfolio/src/components/LeftNavbar/LeftNavbar.js
@@ -1,4 +1,8 @@
 import './LeftNavbar.modules.css';
+import { FaGithubAlt } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { MdOutlineContactPage } from "react-icons/md";
+import { CgTranscript } from "react-icons/cg";
 
 const LeftNavbar = ({sections, activeSection}) => {
     return (
@@ -7,13 +11,34 @@ const LeftNavbar = ({sections, activeSection}) => {
                 {sections.map(section => {
                     const isActive = section.id === activeSection;
                 return (
-                        <li key={section.id} className='sectionLi'>
-                            <img src={isActive ? section.iconWhite : section.icon} style={{height: 28}} alt='loading...' ></img>
-                            <p style={{fontSize: 20, color: isActive ? 'white' : '#B0B0B0'}}>{section.label}</p>
+                        <li key={section.id} className='sectionLi' style={{outline: isActive? '1px solid #7A7A7A' : 'none', boxShadow: isActive? '0 4px 8px rgba(0, 0, 0, 0.1), 0 2px 4px rgba(0, 0, 0, 0.05)' : 'none'}}>
+                            <img src={isActive ? section.iconWhite : section.icon} style={{height: isActive? 30 : 28}} alt='loading...' ></img>
+                            <p style={{fontSize: 20, color: isActive ? 'white' : '#7A7A7A'}}>{section.label}</p>
                         </li>
                     );
                 })}
             </ul>
+            <footer className='leftFooter'>
+                <h2 style={{color: '#B0B0B0', fontFamily: 'Poppins', fontWeight: 'lighter', fontSize: 17}}>Social Links</h2>
+                <span style={{display: 'flex', gap: '13px'}}>
+                    <div className='linkCard'>
+                        <FaGithubAlt color='white'></FaGithubAlt>
+                    </div>
+                    <div className='linkCard'>
+                        <FaLinkedin color='white'></FaLinkedin>
+                    </div>
+                </span>
+
+                <h2 style={{color: '#B0B0B0', fontFamily: 'Poppins', fontWeight: 'lighter', fontSize: 17}}>Resume & Transcript</h2>
+                <span style={{display: 'flex', gap: '13px'}}>
+                    <div className='linkCard'>
+                        <MdOutlineContactPage color='white'></MdOutlineContactPage>
+                    </div>
+                    <div className='linkCard'>
+                        <CgTranscript color='white'></CgTranscript>
+                    </div>
+                </span>
+            </footer>
         </nav>
     );
 };
