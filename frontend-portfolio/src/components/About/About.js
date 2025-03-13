aa165754d081e0076cd2b64e03d7046e5c28d59d
Hector Quinones <quinonhector1@gmail.com>
2025-03-12 19:33:24 -0500

    begain fixing styling based on resolution size for about section

--- frontend-portfolio/src/components/About/About.js
+++ frontend-portfolio/src/components/About/About.js
@@ -3,6 +3,10 @@ import introIcon from "../../assets/square-user-round.svg";
 import introWhiteIcon from "../../assets/square-user-roundW.svg";
 import eduIcon from '../../assets/university.svg';
 import eduWhiteIcon from '../../assets/universityW.svg';
+import utaLogo from '../../assets/utaLogo.png';
+import { IoIosSpeedometer } from "react-icons/io";
+import { FaGoogleScholar } from "react-icons/fa6";
+import './About.css';
 
 import { useEffect, useState } from "react";
 
@@ -68,29 +72,22 @@ const About = () => {
 
     return (
         
-        <section style={{
-            backgroundColor: '#2C2C2C',
-            minHeight: '100vh',  // Ensures it fills the full viewport height
-            width: '100%',       // Full width
-            display: 'flex',
-            padding: 5,
-            boxSizing: 'border-box' // Ensures padding doesn't overflow
-        }}>
+        <section className="about-container">
             <LeftNavbar sections={aboutSections} activeSection={activeSection}></LeftNavbar>
-            <article style={{marginLeft: '20%', left: 5, position: 'relative', width: '100%', alignItems: 'center'}}>
-                <section style={{ width: '100%', height: '65vh', display: 'flex', padding: 20, maxWidth: '100%', overflowX: 'hidden', boxSizing: 'border-box'}} id="Introduction">
+            <article className="article-container" >
+                <section className="intro-container" id="Introduction">
                     <div style={{ width: '50%', height: '100%', alignItems: 'center', justifyContent: 'center', display: 'flex'}}>
                         <div style={{ width: '70%', height: '70%', marginLeft: 'auto', marginTop: 'auto', marginBottom: 'auto', outline: '1px solid #B0B0B0', borderRadius: 10, marginRight: 40}}>
 
                         </div>
                     </div>
-                    <div style={{ width: '40%', height: '100%', alignItems: 'center', color: 'white'}}>
+                    <div className="intro-box">
                         <h1 style={{ color: 'white', marginLeft: 30, fontFamily: 'Poppins', fontSize: 35, marginBottom: 5, marginTop: '10vh'}}>
                             <span style={{ color: 'white' }}>I'm</span>
                             <span style={{ color: 'white' }}> </span>
                             <span style={{ color: '#89CFF0' }}>Hector Quinones</span>
                         </h1>
-                        <p style={{ color: '#B0B0B0', marginLeft: 30, fontWeight: 'lighter', fontSize: 20, padding: 0}}>
+                        <p className="intro-sub">
                             I'm a student currently exploring my passion for software engineering. I enjoy learning about web development, algorithms, and building projects that solve real-world problems.
                         </p>
                         <h2 style={{color: '#89CFF0', marginLeft: 30, fontFamily: 'Poppins'}}>Additional Info</h2>
@@ -110,33 +107,35 @@ const About = () => {
                 <section style={{width: '100%', height: '65vh', display: 'flex', flexDirection:'column', color: 'white'}} id="Education">
                     <h1 style={{fontSize: 35, marginLeft: 'auto', marginRight: 'auto', fontFamily: 'Poppins', marginBottom: 20}}>Education</h1>
                     <div style={{gap: 50, height: '100%', width: '100%', display: 'flex', flexDirection: 'column', marginTop: 0}}>
-                       <div style={{height: '30%', width: '100%', borderTop: '1px solid transparent', borderBottom: '1px solid transparent', borderImageSource: 'linear-gradient(to right, transparent, #B0B0B0, transparent)', borderImageSlice: 1}}>
-                            <h2 style={{gap: 500, display: 'flex', fontFamily: 'Poppins', color: 'white', margin: 30, marginBottom: 0}}>
-                                <span style={{color: '#89CFF0'}}>University of Texas at Arlington</span>
-                                <span style={{fontWeight: 'lighter'}}>Arlington, TX</span>
-                            </h2>
+                       <div className="education">
+                            <div style={{ width: '30%', display: 'flex', justifyContent: 'center', paddingTop: 50, paddingRight: 20}}>
+                                <img src={utaLogo} style={{height: 150}}></img>
+                            </div>
 
-                            <h2 style={{gap: 500, display: 'flex', color: '#B0B0B0', fontStyle: 'italic', marginLeft: 30, marginTop: 0, fontSize: 18}}>
-                                <span style={{fontWeight: 'lighter'}}>Bachelor of Arts in Computer Science</span>
-                                <span style={{fontWeight: 'lighter'}}>Expected Graduation, Spring 2026</span>
-                            </h2>
+                            <div>
+                                <h1 className="educationHeaders" style={{fontSize: 25, color: '#89CFF0', paddingTop: 30}}>The University of Texas at Arlington</h1>
+                                <h2 className="educationHeaders" style={{fontSize: 20, color: '#B0B0B0', paddingTop: 5}}>Bachelor of Arts in Computer Science</h2>
+                                <div>
+                                    <span className="educationSubInfo">
+                                        <IoIosSpeedometer color="#89CFF0" style={{height: 15, marginTop: 20}}></IoIosSpeedometer>
+                                        <p style={{marginTop: 20, marginBottom: 0, marginLeft: 0}}>Accelerated Program</p>
+                                    </span>
+                                    <p style={{margin: 0, paddingLeft: 27, color:'#B0B0B0'}}>On track to graduate in a 3-year span instead of the traditonal 4.</p>
+                                </div>
+
+                                <div>
+                                    <span className="educationSubInfo">
+                                        <FaGoogleScholar color="#89CFF0" style={{height: 15, marginTop: 20}}></FaGoogleScholar>
+                                        <p style={{marginTop: 20, marginBottom: 0, marginLeft: 0}}>Presedential Scholar</p>
+                                    </span>
+                                    <p style={{margin: 0, paddingLeft: 27, color:'#B0B0B0'}}>Have been honored with a prestigous scholarship for outstanding students.</p>
+                                </div>
+                            </div>
                        </div>
-
-                       <div style={{height: '30%', width: '100%', borderTop: '1px solid transparent', borderBottom: '1px solid transparent', borderImageSource: 'linear-gradient(to right, transparent, #B0B0B0, transparent)', borderImageSlice: 1}}>
-                            <h2 style={{gap: 500, display: 'flex', fontFamily: 'Poppins', color: 'white', marginLeft: 30, marginBottom: 0}}>
-                                <span style={{color: '#89CFF0'}}>Tarrant County College</span>
-                                <span style={{fontWeight: 'lighter'}}>Arlington, TX</span>
-                            </h2>
-
-                            <h2 style={{gap: 500, display: 'flex', color: '#B0B0B0', fontStyle: 'italic', marginLeft: 30, marginTop: 0, fontSize: 18}}>
-                                <span style={{fontWeight: 'lighter'}}>Associate's in Liberal Arts</span>
-                                <span style={{fontWeight: 'lighter'}}>Expected Graduation, Spring 2026</span>
-                            </h2>
-                       </div>
-                         
+                                                
                     </div>
                 </section>
-                <section style={{width: '100%', height: '65vh', display: 'flex', backgroundColor: 'yellow'}}></section>
+                {/* <section style={{width: '100%', height: '65vh', display: 'flex', backgroundColor: 'yellow'}}></section> */}
             </article>
         </section>
     );
--- frontend-portfolio/src/components/LeftNavbar/LeftNavbar.js
+++ frontend-portfolio/src/components/LeftNavbar/LeftNavbar.js
@@ -6,7 +6,7 @@ import { CgTranscript } from "react-icons/cg";
 
 const LeftNavbar = ({sections, activeSection}) => {
     return (
-        <nav style={{ backgroundColor: '#3A3A3A', width: '20%', height: '91vh', position: 'fixed', left: 5, borderRadius: 10, boxShadow: '6px 0 16px rgba(0, 0, 0, 0.5), -6px 0 16px rgba(0, 0, 0, 0.5), 6px 0 20px rgba(255, 255, 255, 0.2)'}}>
+        <nav className='leftNavbar'>
             <ul className='sectionList'>
                 {sections.map(section => {
                     const isActive = section.id === activeSection;
