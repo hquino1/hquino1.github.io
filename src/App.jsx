77282c8768dbb78ae0d21589fb60d2fda73cdfe2
Hector Quinones <quinonhector1@gmail.com>
2025-05-14 11:03:51 -0500

    minor styling changes to ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react'
 import Navigation from './components/Navigation.jsx'
 import LimitedNavigation from './components/LimitedNavigation.jsx'
 import About from './pages/About.jsx'
@@ -12,6 +10,7 @@ import { TiThMenu } from "react-icons/ti";
 import { MdNightlight } from "react-icons/md";
 import { MdLightMode } from "react-icons/md";
 import { useDarkMode } from "./hooks/useDarkMode.jsx";
+import { IoClose } from "react-icons/io5";
 
 function App() {
   const [showNavbar, setShowNavbar] = useState(false)
@@ -35,10 +34,11 @@ function App() {
 
   return (
     <div className='transition-colors duration-1000 w-full h-full bg-[#F9FAFB] dark:bg-[#0F172A] flex'>
-      <button className='fixed dark:bg-[#1E293B] bg-[#E5E7EB] right-0 mt-5 mr-20 sm:mr-30 h-12 w-12 rounded-lg justify-center cursor-pointer 2xl:hidden z-60 text-[#111827] dark:text-[#F1F5F9]' onClick={changeNav}>
-        <TiThMenu className='mx-auto text-2xl'></TiThMenu>
+      <button className='fixed dark:bg-[#334155] bg-[#D1D5DB] right-0 mt-5 mr-20 sm:mr-30 h-12 w-12 rounded-lg justify-center cursor-pointer 2xl:hidden z-60 text-[#111827] dark:text-[#F1F5F9]' onClick={changeNav}>
+        {!showNavbar && <TiThMenu className='mx-auto text-2xl'></TiThMenu>}
+        {showNavbar && <IoClose className='mx-auto text-3xl'></IoClose>}
       </button>
-      <button className=' dark:bg-[#1E293B] bg-[#E5E7EB] p-1 rounded-full cursor-pointer fixed right-0 mt-5 mr-5 sm:mr-15 h-12 w-12 z-60 text-[#111827] dark:text-[#F1F5F9] flex items-center justify-center' onClick={toggleDarkMode}>
+      <button className=' dark:bg-[#334155] bg-[#D1D5DB] p-1 rounded-full cursor-pointer fixed right-0 mt-5 mr-5 sm:mr-15 h-12 w-12 z-60 text-[#111827] dark:text-[#F1F5F9] flex items-center justify-center' onClick={toggleDarkMode}>
           {!isDark && <MdNightlight size={22}></MdNightlight>}
           {isDark && <MdLightMode size={22}></MdLightMode>}
       </button>
